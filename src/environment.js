a1c14212827d6658426c2085da243af78fa7a80d
shashikumar <skshashi1224@gmail.com>
2025-06-11 05:27:56 +0530

    Final commit before deployment

---
+++ src/environment.js
@@ -0,0 +1,14 @@
+// let IS_PROD = false;
+// const server = IS_PROD
+//     ? "https://apnacollegebackend.onrender.com"
+//     : "http://localhost:5001";
+
+// export default server;
+
+
+let IS_PROD = true;
+const server = IS_PROD
+    ? "https://apna-video-call-backend-0ogg.onrender.com"
+    : "http://localhost:5001";
+
+export default server;
